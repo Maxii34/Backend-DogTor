5468a569a90ce21f788f8fd9bbbc93874d48d0b5
Maxi Ordoñez <exemaxi@outlook.com>
2025-11-12 20:57:53 -0300

    Se agrego la configuracion de estructuta base de rutas.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import Server from "./src/servers/config.js";
+import router from "./src/routers/index.routes.js";
 
 const server = new Server();
 
 server.listen();
+
+server.app.use('/api', router);
 
 export default server;
