117718d4740911e7ba7bc2d7f836b703c14c0aea
Maxi Ordoñez <exemaxi@outlook.com>
2025-11-13 12:32:04 -0300

    Se agregaron las peticiones de borrarTurno, editarTurno, obtenerTurno y se agregaron sus respectivas rutas.

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -5,7 +5,7 @@ export const crearTurno = async (req, res) => {
     //console.log(req);
     const nuevoTurno = new Turnos(req.body);
     await nuevoTurno.save();
-    res.status(201).json({ message: "Turno creado exitosamente"});
+    res.status(201).json({ message: "Turno creado exitosamente" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error al crear el turno" });
@@ -13,19 +13,57 @@ export const crearTurno = async (req, res) => {
 };
 
 export const listarTurnos = async (req, res) => {
-    try {
-        const turnos = await Turnos.find();
-        res.status(200).json(turnos);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error al listar los turnos" });
-    }
-}
+  try {
+    const turnos = await Turnos.find();
+    res.status(200).json(turnos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error al listar los turnos" });
+  }
+};
 
 export const borrarTurno = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const turnoBorrado = await Turnos.findByIdAndDelete(req.params.id);
+    if (!turnoBorrado) {
+      res.status(404).json({ message: "El turno no fue encontrado" });
+    }
+    res.status(200).json({ message: "El turno fue borrado exitosamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error al borrar el turno" });
+  }
+};
+
+export const turnoEditado = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const TurnoAdcualizado = await Turnos.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!TurnoAdcualizado) {
+        res.status(404).json({ message: "El turno no fue encontrado" });
+    }
+    res.status(200).json({ message: "El turno fue editado exitosamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error al editar el turno" });
+  }
+};
+
+export const obtenerTurno = async (req, res) => {
     try {
-        
+        console.log(req.params.id);
+        const turnoObtenido = await Turnos.findById(req.params.id);
+        if (!turnoObtenido) {
+            res.status(404).json({ message: "El turno no fue encontrado" });
+        }
+        res.status(200).json(turnoObtenido);
     } catch (error) {
-        
+        console.error(error);
+        res.status(500).json({ message: "Error al obtener el turno" });
     }
 }
--- src/routers/productos.routes.js
+++ src/routers/productos.routes.js
@@ -1,9 +1,10 @@
 import {  Router } from "express";
-import { crearTurno, listarTurnos } from "../controllers/productos.controllers.js";
+import { crearTurno, listarTurnos, borrarTurno, turnoEditado, obtenerTurno } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 //http://localhost:3000/api/productos/
 router.route("/").post(crearTurno).get(listarTurnos);
+router.route("/:id").delete(borrarTurno).put(turnoEditado).get(obtenerTurno);
 
 export default router;
